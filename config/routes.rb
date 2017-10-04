9e4a2ce040eeced8ce8f9d7c18e237a41a503937
Mohammad Khan <mkhan138@mail.depaul.edu>
2017-10-04 05:20:16 +0000

    my_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
   resources :microposts
   resources :users
+  resources :my_pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'users#index'
+  root 'my_pages#index'
 end
